9a50a92fd0300f493703e3945cff635c9fed9a23
HidAlexis <alexishidale@gmail.com>
2022-10-16 18:58:23 -0300

    Ejercicio tema 3 - Open Bootcamp

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,21 @@
 public class Main {
     public static void main(String[] args) {
         suma(10,24,12);
-        System.out.println("Nuevo mensaje");
-
+        Coche miCoche = new Coche();
+        miCoche.AñadirPuerta();
+        System.out.println(miCoche.cantidadpuertas);
     }
 
     public static void suma(int a, int b, int c){
         int resultado = a + b + c;
         System.out.println(resultado);
     }
+}
 
-    class Coche {
-        public int cantidadPuertas = 0;
-
-        public void AñadirPuerta(){
-            this.cantidadPuertas++;
-        }
-
+class Coche {
+    public int cantidadpuertas = 2;
+    public void AñadirPuerta(){
+        this.cantidadpuertas++;
     }
 
 }
